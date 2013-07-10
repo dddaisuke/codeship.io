a9a623d90630b29c2e4f6f31826664f7448eaa17
Daisuke Yamashita <daisuke@cookpad.com>
2013-07-10 15:44:22 +0900

    New app

---
+++ codeship/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codeship::Application.config.secret_token = '0c5e0b3575513e10c039467030ed4a7d557379709a2504804bccdea54c4b1731411b4582201f43aad68837d511e94a3d9ffab5e63c2d4f5e75e9250457b268b2'
